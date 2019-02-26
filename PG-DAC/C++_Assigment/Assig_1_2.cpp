567c6dba746fbdef52c3e74600a0a37489bb5527
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2019-02-26 23:33:11 +0530

    Update and rename Assign_1_2.cpp to Assig_1_2.cpp

--- PG-DAC/C++_Assigment/Assig_1_2.cpp
+++ PG-DAC/C++_Assigment/Assig_1_2.cpp
@@ -1,10 +1,3 @@
-//============================================================================
-// Name        : CPP_1_2.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
 
 #include <iostream>
 using namespace std;

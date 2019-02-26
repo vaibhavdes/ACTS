24df16cdb67d03552f882b04a83e4826762a9b09
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2019-02-26 23:04:50 +0530

    Update Assig_1_1.cpp

--- PG-DAC/C++_Assigment/Assig_1_1.cpp
+++ PG-DAC/C++_Assigment/Assig_1_1.cpp
@@ -1,11 +1,4 @@
 //Assignment_1 Que_1
-//============================================================================
-// Name        : CPP_1_1.cpp
-// Author      : 
-// Version     :
-// Copyright   : Your copyright notice
-// Description : Hello World in C++, Ansi-style
-//============================================================================
 
 #include <iostream>
 using namespace std;

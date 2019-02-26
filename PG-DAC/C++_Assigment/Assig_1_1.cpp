b10dc923daee141b144869a3a41f206aabe7098b
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2019-02-26 23:04:34 +0530

    Update and rename Assignment1.cpp to Assig_1_1.cpp

---
+++ PG-DAC/C++_Assigment/Assig_1_1.cpp
@@ -0,0 +1,96 @@
+//Assignment_1 Que_1
+//============================================================================
+// Name        : CPP_1_1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+class Student
+{
+//By default everything is private
+private:
+		int rollno;
+		string name;
+		string course;
+public:
+		Student()
+		{
+			rollno = 0;
+		}
+		Student(int r,string name,string c)
+		{
+			rollno = r;
+			this->name = name;
+			course = c;
+		}
+		void accept(int r,string n,string c)
+		{
+			rollno = r;
+			name = n;
+			course = c;
+		}
+		void show()
+		{
+			cout<<"Roll No "<<rollno<<endl;
+			cout<<"Name "<<name<<endl;
+			cout<<"Course "<<course<<endl;
+		}
+		void search(int r,Student s[])
+		{
+			int i;
+			for(i = 0; i < 10; i++)
+			{
+				if(s[i].rollno == r)
+					cout<<"Name "<<s[i].name<<"Course "<<s[i].course;
+			}
+		}
+		//~Student(); //Otherwise it will end the show before starting
+
+};
+void search(int r,Student s[]);
+int main() {
+
+	Student s1;
+	Student s2(23,"Arjun","PG-DAC");
+	Student s3[10];
+
+	string name,cou;
+	int rollno;
+
+	cout<<"Enter Name"<<endl;
+	cin>>name;
+	cout<<"Enter Course"<<endl;
+	cin>>cou;
+	cout<<"Enter Roll"<<endl;
+	cin>>rollno;
+
+	s1.accept(rollno,name,cou);
+	s1.show();
+	s2.show();
+
+	int i;
+	for(i = 0; i < 2; i++)
+	{
+		cout<<"Enter Name"<<endl;
+		cin>>name;
+		cout<<"Enter Course"<<endl;
+		cin>>cou;
+		cout<<"Enter Roll"<<endl;
+		cin>>rollno;
+
+		s3[i].accept(rollno,name,cou);
+	}
+
+	int s;
+	cout<<"Enter Roll Number to Search";
+	cin>>s;
+	s1.search(s,s3);
+
+
+	return 0;
+}
+

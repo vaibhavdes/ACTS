3296bb1ce2850dd8cb05451a3d91b1b9d540e282
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2019-02-27 21:17:29 +0530

    Create date.cpp

---
+++ PG-DAC/C++_Assigment/Assig_1_3/date.cpp
@@ -0,0 +1,100 @@
+#include<iostream>
+#include "student.h"
+using namespace std;
+Dates::Dates()
+{
+	dd = 0;
+	mm = 0;
+	yyyy = 0;
+}	
+Dates::Dates(int d,int m,int y)
+{
+	dd = d;
+	mm = m;
+	this->yyyy = y;
+}
+void Dates::accept(int d,int m,int y)
+{
+	dd = d;
+	mm = m;
+	yyyy = y;
+}
+void Dates::show()
+{
+	cout<<"\n Day "<<dd;
+	cout<<"\n Month "<<mm;
+	cout<<"\n Year "<<yyyy;
+}
+
+void Dates::difference(int d,int m,int y)
+{
+	int tdays,tdays1;
+	int d1,m1,d2,m2;
+
+	tdays = days(m,y);
+	tdays1 = days(mm,yyyy);
+
+	if(y>yyyy)
+	{
+		cout<<"\n"<<yyyy - y<<" years";
+		d1 = d;
+		m1 = m;
+		d2 = dd;
+		m2 = mm;
+	}
+	else
+	{
+		d1 = dd;
+		m1 = mm;
+		d2 = d;
+		m2 = m;
+		cout<<"\n"<<y - yyyy<<" years";
+	}
+
+	int i,j,temp = 0, temp1 = 0;
+	for(i = m1; i<=m2; i++)
+	{
+		for(j = d1; j <= d2; j++)
+		{
+			if(d1<=tdays)
+			{
+				j = 1;
+				continue;
+			}
+			temp++;
+			cout<<"Test"<<j<<temp;
+		}
+		temp1++;
+	}
+	
+	cout <<"\n"<<temp1<<" month";
+	cout <<"\n"<<temp<<" days";
+	
+}
+
+int Dates::days(int m,int y)
+{
+	int tdays;
+	if(y%4 == 0)
+	{
+		if(m == 2)
+			tdays = 29; 
+	}	
+	else
+	{
+		if(m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
+			tdays = 31;
+		else if(m == 4 || m == 6 || m == 9 || m == 1)
+			tdays = 30;
+		else
+			tdays = 28;
+	}
+	return tdays;
+}
+
+/*
+void Date::compare(int d,int m,int y)
+{
+
+}
+*/

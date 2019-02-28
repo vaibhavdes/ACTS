e61fa7e01635fdf407aea55d5908d8dbbaa4cd51
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2019-02-28 23:48:24 +0530

    Update date.cpp

--- PG-DAC/C++_Assigment/Assig_1_3/date.cpp
+++ PG-DAC/C++_Assigment/Assig_1_3/date.cpp
@@ -27,8 +27,86 @@ void Dates::show()
 }
 
 void Dates::difference(int d,int m,int y)
+{	
+	int y_t;
+	int t_y;
+	int d_y = 0,i;
+	int y_d = 0,j;
+	int l,n,total_days = 0,total_months = 0;
+
+	(yyyy%4==0)?(y_t = 366):(y_t = 365); // Start Year
+	(y%4==0)?(t_y = 366):(t_y = 365); // End Year
+
+	//Start Day
+	for(i = mm + 1; i <= 12;i++)
+	{
+		d_y = d_y + days(i,yyyy);
+		total_months = total_months + 1;
+	}
+		d_y = d_y + (days(mm,yyyy) - dd);
+
+	//End Day
+	for(j = 1; j < m ; j++)
+	{
+		y_d = y_d + days(j,y);
+		total_months = total_months + 1;
+	}
+		y_d =  y_d + d;
+
+	if(y>yyyy && y!=yyyy)
+	{
+		for(l = yyyy + 1;l < y; l++)
+		{
+			for(n = 1; n <= 12; n++)	
+			{	
+				total_days = total_days + days(n,l);
+				total_months = total_months + 1;
+			}
+		}
+	}
+	
+	total_days = total_days + y_d + d_y;	
+	
+	cout<<total_days<<" Days\n"<<end;
+	cout<<total_months<<" Months\n"<<endl;		//Unverified
+	cout<<y - yyyy<<" Years\n"<<endl;	
+}
+
+int Dates::days(int m,int y)
 {
-	int tdays,tdays1;
+	int tdays;
+	if(y%4 == 0)
+	{
+		if(m == 2)
+			tdays = 29; 
+	}	
+	else
+	{
+		if(m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
+			tdays = 31;
+		else if(m == 4 || m == 6 || m == 9 || m == 1)
+			tdays = 30;
+		else
+			tdays = 28;
+	}
+	return tdays;
+}
+
+/*
+void Date::compare(int d,int m,int y)
+{
+
+}
+*/
+
+
+
+
+
+/* Older Implementation || difference()
+
+
+int tdays,tdays1;
 	int d1,m1,d2,m2;
 
 	tdays = days(m,y);
@@ -94,37 +172,9 @@ void Dates::difference(int d,int m,int y)
 		}
 		temp1++;
 	}
-	*/
+	
 	
 	
 	cout <<"\n"<<temp1<<" month";
 	cout <<"\n"<<temp<<" days";
-	
-}
-
-int Dates::days(int m,int y)
-{
-	int tdays;
-	if(y%4 == 0)
-	{
-		if(m == 2)
-			tdays = 29; 
-	}	
-	else
-	{
-		if(m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12)
-			tdays = 31;
-		else if(m == 4 || m == 6 || m == 9 || m == 1)
-			tdays = 30;
-		else
-			tdays = 28;
-	}
-	return tdays;
-}
-
-/*
-void Date::compare(int d,int m,int y)
-{
-
-}
 */

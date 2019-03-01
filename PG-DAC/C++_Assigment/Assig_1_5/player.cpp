536c2c12f5c8612a1079bdeaa21534cf68e832a1
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2019-03-01 21:08:57 +0530

    Create player.cpp

---
+++ PG-DAC/C++_Assigment/Assig_1_5/player.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+#include<string>
+#include"Player.h"
+using namespace std;
+Player::Player()
+{
+	age=0;
+	name='\0';
+	country='\0';
+}
+Player::Player(int a,string b,string c)
+{
+	age=a;
+	name=b;
+	country=c;
+}
+void Player::Accept()
+{
+	cout<<"Enter Age, Name, Country of the Player:"<<endl;
+	cin>>age>>name>>country;
+}
+void Player::Display()
+{
+	cout<<age<<"\t"<<name<<"\t"<<country<<endl;
+}
+void Player::Sort(Player *p,int a)
+{
+	int temp_age;
+	string temp;
+	string temp_c;
+	for(int i=0;i<a;i++)
+	{
+		for(int j=i+1;j<a;j++)
+		{
+			if(p[i].age>p[j].age || p[i].age==p[j].age)
+			{
+				temp_age=p[i].age;
+				temp = p[i].name;
+				p[i].age=p[j].age;
+
+				p[i].name = p[j].name;
+				p[j].age=temp_age;
+				p[j].name = temp;
+
+				temp_c = p[i].country;
+				p[i].country = p[j].country;
+				p[j].country = temp_c;
+			}
+		}
+	}
+	for(int k=0;k<a;k++)
+	{
+		p[k].Display();
+	}
+}

8eddcdbe746c00210f75cbaf5159afcf1985005b
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2019-03-02 19:44:03 +0530

    Update main.cpp

--- PG-DAC/C++_Assigment/Assig_1_6/main.cpp
+++ PG-DAC/C++_Assigment/Assig_1_6/main.cpp
@@ -1,1 +1,29 @@
 
+#include<iostream>
+#include<cstring>
+#include "address.h"
+using namespace std;
+int main()
+{
+	bool flag = 0; 
+	
+	Address a(0,"www","aaa","bbb",234);
+	a.display();
+
+	Address b;
+        b.accept();
+        b.display();
+
+	flag = (b == a);
+	if(flag)
+	{
+		cout<<"Same";
+	}
+	else 
+	{
+		cout<<"Different";
+	}	
+
+	return 0;
+}
+

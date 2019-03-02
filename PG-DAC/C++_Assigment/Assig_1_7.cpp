ad5d67cca03e6b2de9188793a54ac105d02cf4fa
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2019-03-02 16:53:51 +0530

    Delete Assig_1_7.cpp

--- PG-DAC/C++_Assigment/Assig_1_7.cpp
+++
@@ -1,6 +0,0 @@
-//Create .h file with all declaration (prototype) of class (data members,member functions)
-//Create .CPP file with Implementation of a Class Method (Include .h file for the same)
-e.g Student::add()
-    {
-    }
-//Include same .h file where main() function is present

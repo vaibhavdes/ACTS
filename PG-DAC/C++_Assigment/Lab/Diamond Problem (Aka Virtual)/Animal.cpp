9eea072f5d7305937d146be40e344547f99ac734
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2019-03-05 15:00:17 +0530

    Create Animal.cpp

---
+++ PG-DAC/C++_Assigment/Lab/Diamond Problem (Aka Virtual)/Animal.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include "Animal.h"
+using namespace std;
+
+
+void Animal::setWeight(int w)
+{
+		weight = w;
+		cout << w;
+}
+void Tiger::setWidth(int w)
+{
+		width = w;
+}
+void Lion::setHeight(int h)
+{
+		height = h;	
+}

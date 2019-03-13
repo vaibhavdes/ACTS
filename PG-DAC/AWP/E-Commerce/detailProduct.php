7e466f614e7a5e02064f02af15e986f4137f2f04
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2019-03-13 21:07:50 +0530

    Create detailProduct.php

---
+++ PG-DAC/AWP/E-Commerce/detailProduct.php
@@ -0,0 +1,57 @@
+<html>
+    <head>
+        <title>FlipKart</title>
+        <link rel="stylesheet" href="css/style.css">
+        <link rel="stylesheet" href="css/style-product.css">
+
+    </head>
+    <body>
+        <div class="head">
+            <p class="head-text"><b>FlipKart</b></p>
+            <div class="search-box">
+                <input type="text" class="search-product" placeholder="Search for products, brands and more...">
+                <svg  class="search-icon" xmlns="http://www.w3.org/2000/svg"><g fill="#2874F1" fill-rule="evenodd"><path class="_2BhAHa" d="m11.618 9.897l4.225 4.212c.092.092.101.232.02.313l-1.465 1.46c-.081.081-.221.072-.314-.02l-4.216-4.203"></path><path class="_2BhAHa" d="m6.486 10.901c-2.42 0-4.381-1.956-4.381-4.368 0-2.413 1.961-4.369 4.381-4.369 2.42 0 4.381 1.956 4.381 4.369 0 2.413-1.961 4.368-4.381 4.368m0-10.835c-3.582 0-6.486 2.895-6.486 6.467 0 3.572 2.904 6.467 6.486 6.467 3.582 0 6.486-2.895 6.486-6.467 0-3.572-2.904-6.467-6.486-6.467"></path></g></svg>
+            </div>
+            <div class="head-menu" >
+                <a href="login.html">
+                <p class="menu-items" style=" margin-left: 8%; ">Login</p>
+                </a>
+                <p class="menu-items" style=" margin-left: 16%; ">Register</p>
+                <p class="menu-items" style=" margin-left: 24%; ">Cart</p>
+            </div>
+        </div>
+
+        <div class="product">
+             <div  class="product-left">
+                <div class="product-content">
+                    <img src="images/sample_image.jpg">
+                </div>
+                <button style="background: #ff9f00;">Add to Cart</button>
+                <button style="background: #fb641b;">Buy Now</button>
+            </div>
+            <div class="product-right">
+               <div class="product-content">
+                    <p class="title">Miss & Chief High Speed Stickerless 3x3 Magic Rubik Cube Puzzle Game Toy  (1 Pieces)</p>
+                    <p class="title" style="font-size: 32px;">â‚¹116</p> 
+                    <div class="feature">
+                        <div class="heading" style="color:#878787;"> Highlights </div>
+                        <div class="features" style="margin-left:15%;">
+                        <table>
+                            <tr>
+                                <td>
+                            <li class="features">Material: Plastic</li>
+                            <li class="features">Minimum Age: 5 years</li>
+                            <li class="features">Skillset: Color & Shape Recognition</li>
+                                </td>
+                                <td>
+                            <li class="features">10 Days Replacement Policy?</li>
+                            <li class="features">Cash on Delivery available?</li>
+                            <li class="features">Free shippingâ‚¹40</li>
+                                </td>
+                            </tr>
+                        </table>
+                        </div> 
+                    </div>
+               </div>         
+            </div>
+        </div>

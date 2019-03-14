8a4bf8d565cdbd0d2702ca320cd630fd360fca35
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2019-03-14 20:54:55 +0530

    Add files via upload

---
+++ PG-DAC/AWP/E-Commerce/pages/header.php
@@ -0,0 +1,53 @@
+    <div class="head">
+            <div class="head-text">
+            <a href="index.php"><b>FlipKart</b></a>
+            </div>
+            <div class="search-box">
+                <input type="text" class="search-product" placeholder="Search for products, brands and more...">
+                <svg  class="search-icon" xmlns="http://www.w3.org/2000/svg"><g fill="#2874F1" fill-rule="evenodd"><path class="_2BhAHa" d="m11.618 9.897l4.225 4.212c.092.092.101.232.02.313l-1.465 1.46c-.081.081-.221.072-.314-.02l-4.216-4.203"></path><path class="_2BhAHa" d="m6.486 10.901c-2.42 0-4.381-1.956-4.381-4.368 0-2.413 1.961-4.369 4.381-4.369 2.42 0 4.381 1.956 4.381 4.369 0 2.413-1.961 4.368-4.381 4.368m0-10.835c-3.582 0-6.486 2.895-6.486 6.467 0 3.572 2.904 6.467 6.486 6.467 3.582 0 6.486-2.895 6.486-6.467 0-3.572-2.904-6.467-6.486-6.467"></path></g></svg>
+            </div>
+            <div class="head-menu" >
+                
+                <?php
+                if(!empty($_SESSION['login_status']))
+                {
+                    if($_SESSION['login_status'] == 1)
+                    {
+                        echo'
+                        <a href="profile.php">
+                        <p class="menu-items" style=" margin-left: 8%; ">Profile</p>
+                        </a>
+                        <p class="menu-items" style="margin-left: 16%; ">Cart</p>
+                        <a href="logout.php">
+                        <p class="menu-items" style=" margin-left: 24%; ">Logout</p>
+                        </a>
+                        ';
+                    }
+                    else
+                    {
+                        echo'
+                        <a href="login.php">
+                        <p class="menu-items" style=" margin-left: 8%; ">Login</p>
+                        </a>
+                        <a href="register.php">
+                        <p class="menu-items" style=" margin-left: 16%; ">Register</p>
+                        </a>
+                        <p class="menu-items" style="margin-left: 24%; ">Cart</p>
+                        ';
+                    }
+                }
+                else
+                {                   
+                    echo'
+                    <a href="login.php">
+                    <p class="menu-items" style=" margin-left: 8%; ">Login</p>
+                    </a>
+                    <a href="register.php">
+                    <p class="menu-items" style=" margin-left: 16%; ">Register</p>
+                    </a>
+                    <p class="menu-items" style="margin-left: 24%; ">Cart</p>
+                    '; 
+                }
+                ?>
+            </div>
+        </div>

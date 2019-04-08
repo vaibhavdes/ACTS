8bea52f76631f65e10fbedfc88fd499094a4f1b9
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2019-04-08 22:23:54 +0530

    Create main.cpp

---
+++ PG-DAC/DS/Assignment/ass1_3/main.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	//USED FOR QUEUE,STACK
+
+	int *a;
+	a = new int[5];
+	
+	a[0] = 21;
+	cout<<a[0]<<endl;
+
+	delete[] a;
+	
+
+	//USED FOR GRAPH
+
+
+	//1) Fixed Coloumn - 
+	int **b;
+		
+		//Rows
+		b = new int*[5];
+	
+		//For Each Coloumns
+		//Can use (For Loop 5) times
+		b[1] = new int[5];
+
+		b[1][1] = 23;
+
+		cout<<b[1][2]<<endl;
+		
+		//Remove For Each Coloumn
+		delete[] b[1];
+
+	delete[] b;
+
+	
+}

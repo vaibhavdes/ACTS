141189391be3fe9987e7e5f360fe71a2d5087d48
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2019-04-12 16:52:26 +0000

    Create main.cpp

---
+++ PG-DAC/DS/Structure/PriorityQueue/main.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<cstdlib>
+#include "queue.h"
+using namespace std;
+
+int main()
+{
+	queue q;
+
+	q.create();
+	
+	int option=1;
+	
+	while(option!=0)
+	{
+		cout<<"1.Insert \n2.Remove \n3.Display \n0.Exit\n"<<endl;
+		cin>>option;
+
+		switch(option)
+		{
+			case 1:
+				q.insert();
+				break;
+			case 2:
+				q.remove_max();
+				break;
+			case 3:
+				q.display();
+				break;
+			case 0:
+				exit(0);
+		}
+	}	
+}

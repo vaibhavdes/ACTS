c178e212cb5f555fa85e7ae7c3790b53fb54051d
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2019-04-14 21:05:14 +0530

    Create 1.main.ts

---
+++ PG-DAC/MEANSTACK/Pre/TypeScript/Intermediate/8.Modules/1.main.ts
@@ -0,0 +1,8 @@
+
+    //Sample: import { Point } from '@angular/core';
+    
+    import { Point } from './point';
+    //Multiple Import then separate by Comma
+
+    let p = new Point(1,2);
+    p.draw();

8af222d63107d110306e4a543a9f08a38a0bac15
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2019-04-14 20:54:50 +0530

    Create 3.variable_type.ts

---
+++ PG-DAC/MEANSTACK/Pre/TypeScript/Basic/3.variable_type.ts
@@ -0,0 +1,15 @@
+//Check Compiled main.js Code
+    
+    // let a: number; a:boolean; a: string; a: any; a: array[] = {1,2,3};
+    
+    let a;            //Type: any
+    a = 1;
+    a = true;
+    a = 'a';
+
+
+
+/*  Try Executing
+  let count = 5;
+  count = 'a';            //Error - Due to Strong Typing
+*/

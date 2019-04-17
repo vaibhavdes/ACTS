d629e3746b469a1b4979f2581d3235944e7b2b71
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2019-04-17 08:22:15 +0530

    Create HelloTwo.java

---
+++ PG-DAC/JAVA/Core/Assignment/Day_1/Try_This/HelloTwo.java
@@ -0,0 +1,25 @@
+//Try-This		//ERROR
+/*	public class HelloThree
+	{
+		//Try-3		//WORKING
+		public static void main(String[] args)
+		{
+			System.out.println("Hello World");
+		}
+	}	*/
+	
+//Try This
+class A
+{
+	public static void main(String[] args)
+		{
+			System.out.println("Hello World");
+		}
+}
+public class HelloTwo
+{
+	public static void main(String[] args)
+		{
+			System.out.println("Hello World");
+		}
+}

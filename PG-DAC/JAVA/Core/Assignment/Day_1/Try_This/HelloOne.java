7bdc1d5b103577ce21032df6b11f991725ffb0b3
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2019-04-17 08:23:06 +0530

    Create HelloOne.java

---
+++ PG-DAC/JAVA/Core/Assignment/Day_1/Try_This/HelloOne.java
@@ -0,0 +1,23 @@
+//
+class HelloSample
+{
+	//Try-1		//RUNTIME-ERROR
+/*	public static void Main(String[] args)
+	{
+		System.out.println("Hello World");
+	}	*/
+	
+	//Try-2		//RUNTIME-ERROR
+/*	public static void main(String args)
+	{
+		System.out.println("Hello World");
+	}	*/
+	
+	//Try-3		//WORKING
+	public static void main(String[] args)
+	{
+		System.out.println("Hello World");
+	}
+}
+
+//Compiler Do not Checks for main method

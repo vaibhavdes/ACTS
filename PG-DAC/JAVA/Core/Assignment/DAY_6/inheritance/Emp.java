f0344b8344a609b0fdcc6dc26f0ee5b73c5594b3
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2019-04-26 20:14:49 +0530

    Create Emp.java

---
+++ PG-DAC/JAVA/Core/Assignment/DAY_6/inheritance/Emp.java
@@ -0,0 +1,43 @@
+package inheritance;
+
+public abstract class Emp {
+	private int id;
+	private String name;
+	private String email;
+	private String deptId;
+	private double basic;
+	private static int count;
+	
+	static {
+		count = 100;
+	}
+	
+	public Emp(String name,String email,String deptId,double basic)
+	{
+		count++;
+		this.id = count;
+		this.name = name;
+		this.email = email;
+		this.deptId = deptId;
+		this.basic = basic;
+	}
+	
+	public double getBasic()
+	{
+		return basic;
+	}
+
+	public void setBasic(double basic)
+	{
+		this.basic = basic;
+	}
+	
+	public abstract void computeNetSalary();
+
+	@Override
+	public String toString()
+	{
+		return "ID:" + id + " Name:" + name + " Email:" + email;
+	}
+	
+}

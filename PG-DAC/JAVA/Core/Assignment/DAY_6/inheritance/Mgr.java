ebee833552285b922c435be1cde9ecee107aab79
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2019-04-26 20:15:05 +0530

    Create Mgr.java

---
+++ PG-DAC/JAVA/Core/Assignment/DAY_6/inheritance/Mgr.java
@@ -0,0 +1,22 @@
+package inheritance;
+
+public class Mgr extends Emp{
+	private int perfBonus;
+	
+	public Mgr(String name,String email,String deptId,double basic,int perfBonus)
+	{
+		super(name,email,deptId,basic);
+		this.perfBonus = perfBonus;
+	}
+	
+	public void computeNetSalary()
+	{
+		System.out.println("Sal: " + super.getBasic() + perfBonus);
+	}
+	
+	@Override
+	public String toString()
+	{
+		return "Details " + super.toString() + " PerfBonus:" + perfBonus;
+	}
+}

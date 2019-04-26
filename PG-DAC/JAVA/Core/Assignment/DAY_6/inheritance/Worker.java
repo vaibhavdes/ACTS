8aeb326c5f6b3d52e1bf24471aa266ca5ffd6f21
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2019-04-26 20:15:28 +0530

    Create Worker.java

---
+++ PG-DAC/JAVA/Core/Assignment/DAY_6/inheritance/Worker.java
@@ -0,0 +1,30 @@
+package inheritance;
+
+public class Worker extends Emp{
+		private int hoursWorked;
+		private int hourlyRate;
+		
+		public Worker(String name,String email,String deptId,double basic,int hoursWorked,int hourlyRate)
+		{
+			super(name,email,deptId,basic);
+			this.hourlyRate = hourlyRate;
+			this.hoursWorked = hoursWorked;
+		}
+		
+		public void computeNetSalary()
+		{
+			System.out.println("Sal: " + super.getBasic() + (hoursWorked * hourlyRate));
+		}
+	
+		public int getRate()
+		{
+			return hourlyRate;
+		}
+		
+		@Override
+		public String toString()
+		{
+			return "Details " + super.toString() + " Hours Worked:" + hoursWorked + " Hourly Rate:" + hourlyRate;
+		}
+		
+}

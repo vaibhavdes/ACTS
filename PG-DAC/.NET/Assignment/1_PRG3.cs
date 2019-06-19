45eaf2fd14d4abdc078e4c279a96f58e54856655
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2019-06-19 09:36:59 +0530

    Create 1_PRG3.cs

---
+++ PG-DAC/.NET/Assignment/1_PRG3.cs
@@ -0,0 +1,11 @@
+ /*
+            int min, max;
+            Console.WriteLine("Enter Min and Max Number");
+            min = Convert.ToInt32(Console.ReadLine());
+            max = Convert.ToInt32(Console.ReadLine());
+
+            if (min > max)
+                Console.WriteLine("Max is : " + min + " Min is : " + max);
+            else
+                Console.WriteLine("Max is : " + max + " Min is : " + min);
+ */

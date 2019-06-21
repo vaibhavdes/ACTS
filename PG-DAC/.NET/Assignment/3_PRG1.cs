f8a14e9d40254b50fb32994266b858d3d43fbf0e
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2019-06-21 08:41:52 +0530

    Update 3_PRG1.cs

--- PG-DAC/.NET/Assignment/3_PRG1.cs
+++ PG-DAC/.NET/Assignment/3_PRG1.cs
@@ -6,6 +6,130 @@ using System.Threading.Tasks;
 
 namespace ConsoleApplication2
 {
+    
+    class Book
+    {
+        private static int count = 0;
+        private int id;
+        private string title;
+        private string author;
+        private double price;
+        private int pages;
+
+        public Book(string title,string author,double price,int pages)
+        {
+            count++;
+            this.id = count;
+            this.title = title;
+            this.author = author;
+            this.price = price;
+            this.pages = pages;
+        }
+
+        public int Id
+        {
+            get
+            {
+                return id;
+            }
+
+            set
+            {
+                id = value;
+            }
+        }
+
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+
+            set
+            {
+                title = value;
+            }
+        }
+
+        public string Author
+        {
+            get
+            {
+                return author;
+            }
+
+            set
+            {
+                author = value;
+            }
+        }
+
+        public double Price
+        {
+            get
+            {
+                return price;
+            }
+
+            set
+            {
+                price = value;
+            }
+        }
+
+        public int Pages
+        {
+            get
+            {
+                return pages;
+            }
+
+            set
+            {
+                pages = value;
+            }
+        }
+    }
+
+    class EBook :  Book
+    {
+        private int size;
+        private string format;
+
+        public EBook(string title, string author, double price, int pages,int size,string format) : base(title, author, price, pages)
+        { 
+            this.size = size;
+            this.format = format;
+        }
+
+        public int Size
+        {
+            get
+            {
+                return size;
+            }
+
+            set
+            {
+                size = value;
+            }
+        }
+
+        public string Format
+        {
+            get
+            {
+                return format;
+            }
+
+            set
+            {
+                format = value;
+            }
+        }
+    }
+    
     class Program
     {
         static void Main(string[] args)

2970805bdc7b6a56cafec065252488483ac2fbb6
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2019-06-24 12:17:24 +0530

    Create Calculator.cs

---
+++ PG-DAC/.NET/Concept/Assembly_DLL_Addition/Calculator.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Calculator
+    {
+        public void add(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        } 
+    }
+}

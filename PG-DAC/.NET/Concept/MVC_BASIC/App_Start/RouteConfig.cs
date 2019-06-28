679847cac0447b9f16ffdbd322de8c4c19ef2b11
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2019-06-28 12:49:04 +0530

    Create RouteConfig.cs

---
+++ PG-DAC/.NET/Concept/MVC_BASIC/App_Start/RouteConfig.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace PahilaMVC
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Bank", action = "Index", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+                name: "Login",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Bank", action = "Login", id = UrlParameter.Optional }
+            );
+
+            routes.MapRoute(
+              name: "NewUser",
+              url: "{controller}/{action}/{id}",
+              defaults: new { controller = "Bank", action = "NewUser", id = UrlParameter.Optional }
+          );
+        }
+    }
+}

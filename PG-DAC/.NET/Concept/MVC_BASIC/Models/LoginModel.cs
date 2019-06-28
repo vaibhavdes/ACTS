0dde965d467cc444a8a526364334dbdbdb71ce1e
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2019-06-28 12:51:25 +0530

    Create LoginModel.cs

---
+++ PG-DAC/.NET/Concept/MVC_BASIC/Models/LoginModel.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PahilaMVC.Models
+{
+    public class LoginModel
+    {
+        public string LoginID { get; }
+        public string Password { get; }
+        public string LoginType { get; }
+    }
+}

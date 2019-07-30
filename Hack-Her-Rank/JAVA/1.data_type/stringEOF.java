63d7fe44b903ef17c8e5c75ef8a60bda14ea70a5
Vaibhav Bhausaheb Kurkute <vaibhavdesign@gmail.com>
2019-07-30 10:09:19 +0530

    Create stringEOF.java

---
+++ Hack-Her-Rank/JAVA/1.data_type/stringEOF.java
@@ -0,0 +1,52 @@
+"In computing, End Of File (commonly abbreviated EOF) is a condition in a computer operating system where no more data can be read from a data source." — (Wikipedia: End-of-file)
+
+The challenge here is to read  lines of input until you reach EOF, then number and print all  lines of content.
+
+Hint: Java's Scanner.hasNext() method is helpful for this problem.
+
+Input Format
+
+Read some unknown  lines of input from stdin(System.in) until you reach EOF; each line of input contains a non-empty String.
+
+Output Format
+
+For each line, print the line number, followed by a single space, and then the line content received as input.
+
+Sample Input
+
+Hello world
+I am a file
+Read me until end-of-file.
+Sample Output
+
+1 Hello world
+2 I am a file
+3 Read me until end-of-file.
+
+
+#CODE
+
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        String s;
+        int i = 0;
+        while(sc.hasNext())
+        {
+            i = i + 1;
+            s=sc.nextLine();
+            System.out.println(i + " " + s);
+        }
+        
+    }
+}
+
